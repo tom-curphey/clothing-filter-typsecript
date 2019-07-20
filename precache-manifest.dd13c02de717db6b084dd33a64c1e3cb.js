5e5f7a64e80bea8dd79fdc2061b1d408f5b84370
muppet-training <tomcurphey12@gmail.com>
2019-07-20 13:29:42 +1000

    Updates

---
+++ precache-manifest.dd13c02de717db6b084dd33a64c1e3cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fb7fc7d4978686b9a101ddf550e580f",
+    "url": "/clothing-filter-typescript/index.html"
+  },
+  {
+    "revision": "a6bedd0b309d2d9185d2",
+    "url": "/clothing-filter-typescript/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/clothing-filter-typescript/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a6bedd0b309d2d9185d2",
+    "url": "/clothing-filter-typescript/static/js/main.d5a580b6.chunk.js"
+  },
+  {
+    "revision": "c77b9636071d9b16d3df",
+    "url": "/clothing-filter-typescript/static/js/runtime~main.a2dac35d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clothing-filter-typescript/static/media/logo.5d5d9eef.svg"
+  }
+]);
